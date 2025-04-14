5ad7b01ab22733a069f1a8c4dce1e23338258ec9
touero <touer0018@gmail.com>
2025-04-14 17:45:47 +0800

    feat: support add new item by swift

--- main.swift
+++ main.swift
@@ -2,11 +2,90 @@ import Foundation
 
 let currentDir = FileManager.default.currentDirectoryPath
 let readmePath = currentDir + "/README.md"
-print(readmePath)
 
 do {
-    let fileContents = try String(contentsOfFile: readmePath, encoding: .utf8)
-    print(fileContents)
+    var fileContents = try String(contentsOfFile: readmePath, encoding: .utf8)
+    print("\(fileContents)\n")
 } catch {
     print("读取文件失败: \(error)")
+    exit(0)
+}
+
+print("输入表格数据, 每次按提示输入, 按顺序输入以下内容: 1.输入名称, 2. 安装链接, 3. 描述, 4. 依赖, 5. 是否需要添加到自动化, 输入 'exit' 或者 'quit' 退出程序.\n")
+
+struct LineWritten {
+    var name: String = ""
+    var installUrl: String = ""
+    var description: String = ""
+    var dependency: String = "None"
+    var isAddAuto: String = "✘"
+
+    func writeFile() {
+        let line = "| [\(name)](\(installUrl)) | \(description) | \(dependency) | \(isAddAuto) |\n"
+        if let fileHandle = try? FileHandle(forWritingTo: URL(fileURLWithPath: readmePath)) {
+            fileHandle.seekToEndOfFile()
+            if let data = line.data(using: .utf8){
+                fileHandle.write(data)
+            }
+            fileHandle.closeFile()
+            print("write to README.md success, you can continue or exit")
+        } else {
+            print("write to README.md fail, will exit")
+            exit(0)
+        }
+    }
+}
+
+func checkExit(input: String) {
+    if input == "exit" || input == "quit" {
+        print("\n程序退出");
+        exit(0)
+    }
+}
+
+while true {
+    var lineWritten = LineWritten()
+        print("1. 输入名称: ", terminator:"")
+    if let input = readLine(), !input.isEmpty {
+        checkExit(input: input)
+        lineWritten.name = input
+    } else {
+        print("名称为空")
+        continue
+    }
+    print("2. 输入安装链接: ", terminator:"")
+    if let input = readLine(), !input.isEmpty {
+        checkExit(input: input)
+        lineWritten.installUrl = input
+    } else {
+        print("安装链接为空")
+        continue
+    }
+    print("3. 输入描述: ", terminator:"")
+    if let input = readLine(), !input.isEmpty {
+        checkExit(input: input)
+        lineWritten.description = input
+    } else {
+        print("描述为空")
+        continue
+
+    }
+    print("4. 输入依赖 (不输入默认为None): ", terminator:"")
+    if let input = readLine(), !input.isEmpty {
+        lineWritten.dependency = String(input)
+    }
+    print("5. 是否需要添加到自动化 (1: ✔, 2: ✘, 不输入默认为✘): ", terminator:"")
+    if let input = readLine(), !input.isEmpty {
+        var result: String
+        if input == "1" {
+            result = "✔"
+        } else if input == "2" {
+            result = "✘"
+        } else {
+            print("不受支持的参数，默认设置为 ✘");
+            result = "✘"
+        }
+        lineWritten.isAddAuto = result
+    }
+    lineWritten.writeFile()
 }
