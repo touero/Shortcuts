f7afdac5825a04f2e93ced7221fa063514a0229f
touero <touer0018@gmail.com>
2025-04-16 11:59:45 +0800

    feat: support command args and remove someone line
    support include:
     - command line args include: help, version, remove
     - remove someone line

--- main.swift
+++ main.swift
@@ -1,19 +1,50 @@
 import Foundation
 
+let arguments = CommandLine.arguments
 let currentDir = FileManager.default.currentDirectoryPath
 let readmeURL = URL(fileURLWithPath: currentDir).appendingPathComponent("README.md")
 
+if arguments.count == 2 {
+    let argument = arguments[1]
+    if argument == "--help" || argument == "-h" {
+        print("-v or --version: 查看当前版本")
+        print("-h or --help: 查看帮助")
+        print("-r or --remove: 删除某一行")
+        exit(0)
+    } else if argument == "--version" || argument == "-v"{
+        print("Version 0.0.1")
+        exit(0)
+    } else if argument == "--remove" || argument == "-r"{
+        removeLine(readmeURL: readmeURL)
+        exit(0)
+    }
+}
 
-do {
-    let fileContents = try String(contentsOf: readmeURL, encoding: .utf8)
-    print("\(fileContents)\n")
-} catch {
-    print("读取文件失败: \(error)")
-    exit(1)
+func removeLine(readmeURL: URL){
+    do {
+        let fileContents = try String(contentsOf: readmeURL, encoding: .utf8)
+        var lines = fileContents.split(separator: "\n").map { String($0) }
+        let lineToShow = lines.dropFirst(4)
+        for (index, line) in lineToShow.enumerated() {
+            print("\(index + 5): \(line)")
+        }
+        print("请输入删除的行号: ", terminator: "")
+        if let input = readLine(), let lineToRemove = Int(input), lineToRemove >= 5, lineToRemove <= lines.count{
+            let indexToRemove = lineToRemove - 1
+            lines.remove(at: indexToRemove)
+            let newContent = lines.joined(separator: "\n")
+            try newContent.write(to: readmeURL, atomically: true, encoding: .utf8)
+            print("行 \(lineToRemove) 已成功删除")
+        } else {
+            print("无效行号或输入, 程序退出")
+        }
+    } catch {
+        print("读取文件失败: \(error)")
+        exit(1)
+    }
 }
 
 print("输入表格数据, 每次按提示输入, 按顺序输入以下内容: 1.输入名称, 2. 安装链接, 3. 描述, 4. 依赖, 5. 是否需要添加到自动化, 输入 'exit' 或者 'quit' 退出程序.\n")
-
 struct LineWritten {
     var name: String = ""
     var installUrl: String = ""
